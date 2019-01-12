61f04407ec35b2d096c05d6f5e570c312e279bfe
wubin1234 <799639771@qq.com>
2019-01-12 10:17:47 +0800

    第五组

--- Five_Cars/src/main/java/tech/servlet/login/LoginController.java
+++ Five_Cars/src/main/java/tech/servlet/login/LoginController.java
@@ -1,6 +1,5 @@
-package tech.servlet.register;
+package tech.servlet.login;
 
-import com.sun.net.httpserver.HttpsServer;
 import tech.aistar.dao.IUserDao;
 import tech.aistar.dao.impl.UserDaoImpl;
 import tech.aistar.entity.User;
@@ -15,10 +14,8 @@ import java.io.IOException;
 /**
  * Created by wubin on 2019/1/11.
  */
-
-@WebServlet(urlPatterns = "/register")
-public class RegisterController extends HttpServlet {
-    @Override
+@WebServlet(urlPatterns = "/login")
+public class LoginController extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //post请求需要进行手动的配置来处理中文乱码
         //并且是在接受数据之前进行设置
@@ -27,29 +24,27 @@ public class RegisterController extends HttpServlet {
         //获取表单中的数据
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        String phone = req.getParameter("phone");
-        String address = req.getParameter("address");
-        if((username =="")||(password =="")||(phone =="")||(address == "")){
-            resp.sendRedirect("/Five_Cars/jsp/one/registerfail.jsp");
-            return;
-        }
 //        System.out.println("username:"+username);
 //        System.out.println("password:"+password);
-//        System.out.println("phone:"+phone);
-//        System.out.println("address:"+address);
+
 
         IUserDao userDao = new UserDaoImpl();
+        User user = userDao.getByUserName(username);
+//        System.out.println(user.getPassword());
+        //用户名不存在
+        if(user == null){
+            resp.sendRedirect("/Five_Cars/jsp/one/loginfail1.jsp");
+            return;
+        }
 
-        //写入数据库
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setPhone(phone);
-        user.setAddress(address);
-        userDao.save(user);
+        //登录成功
+        if(password.equals(user.getPassword())){
+            resp.sendRedirect("/Five_Cars/jsp/one/index.jsp");
+            return;
+        }
 
-        //重定向
-        resp.sendRedirect("/Five_Cars/jsp/one/login.jsp");
+        //密码错误
+        resp.sendRedirect("/Five_Cars/jsp/one/loginfail2.jsp");
+//        System.out.println(user.getPassword());
     }
 }
-
---
+++ Five_Cars/src/main/webapp/jsp/one/loginfail2.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wubin
+  Date: 2019/1/12
+  Time: 9:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录失败</title>
+</head>
+<body>
+
+</body>
+<script>
+    alert("密码错误，请重新登录！")
+    location = "/Five_Cars/jsp/one/login.jsp";
+</script>
+</html>
